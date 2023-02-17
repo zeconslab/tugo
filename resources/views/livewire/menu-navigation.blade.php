8769559810f8196e81631272e96552bb3aae23b1
Raul Pimentel <thezontrox@gmail.com>
2023-02-17 15:34:10 -0600

    Initial Commit

---
+++ resources/views/livewire/menu-navigation.blade.php
@@ -0,0 +1,13 @@
+<div class="bg-white">
+    <div class="container flex justify-between">
+        <div class="flex gap-4">
+            <p>numero</p>
+            <p>direccion</p>
+            <p>Correo</p>
+        </div>
+        <div class="flex gap-4">
+            <p>moneda</p>
+            <p>Mi cuenta</p>
+        </div>
+    </div>
+</div>
